5c09f0d22aa43e031040461df094d250b98223cb
suzanaduran <43820790+suzanaduran@users.noreply.github.com>
2022-12-11 18:34:50 -0500

    Updated connbike_10.py. Why 3 GPIO.output?

--- python_scripts/connbike_10.py
+++ python_scripts/connbike_10.py
@@ -1,6 +1,12 @@
-#! /usr/bin/python
-# Written by Dan Mandle http://dan.mandle.me September 2012
-# License: GPL 2.0
+#################################################################
+#! /usr/bin/python                                              #
+# Author: Suzana Duran Bernardes                                #
+# Adapted from Dan Mandle http://dan.mandle.me September 2012   #
+# Contributors: Abdullah Kurkcu                                 #
+# License: GPL 2.0                                              #
+#################################################################
+
+# Libraries
 import RPi.GPIO as GPIO
 import os
 from gps import *
@@ -17,22 +23,23 @@ from alerts import *
 from mpu6050 import mpu6050 as mpu
 from getmac import get_mac_address
 
-gpsd = None #seting the global variable
-log_rate = 0.5 # 1 second
-os.system('clear') #clear the terminal (optional)
+gpsd = None # Seting the global variable
+log_rate = 0.5 # 0.5 seconds
+os.system('clear') # Clear the terminal (optional)
 GPIO.setmode(GPIO.BCM)
-range_limit = 5000 #for the ultrasonic sensor range
-LOG_FILE = '/connbike_test.db'
+range_limit = 5000 # For the ultrasonic sensor range. Set 5000 rather than 400 because high readings can be due materials such as fabrics or trees rather than truly out-of-range.
+LOG_FILE = '/connbike.db' # File for storing data and messages
 DEBUG = True
 
-mac = get_mac_address(interface="eth0")
-sensor = mpu(0x68, bus=0)
+mac = get_mac_address(interface="eth0") # Get mac address of the unit
+sensor = mpu(0x68, bus=4) # Calling the MPU6050 sensor
 
-TRIG_r = 23 
-ECHO_r = 24
-TRIG_l = 17 
-ECHO_l = 27
+TRIG_r = 23 # Trigger GPIO for ultrasonic sensor on the right
+ECHO_r = 24 # Echo GPIO for ultrasonic sensor on the right
+TRIG_l = 17 # Trigger GPIO for ultrasonic sensor on the left
+ECHO_l = 27 # Echo GPIO for ultrasonic sensor on the left
 
+# Setting up the in and out GPIOs of the ultrasonic sensor
 GPIO.setup(TRIG_r,GPIO.OUT)
 GPIO.setup(ECHO_r,GPIO.IN) 
 GPIO.setup(TRIG_l,GPIO.OUT)
@@ -56,15 +63,15 @@ MESSAGE_LEVELS = {
 class GpsPoller(threading.Thread):
   def __init__(self):
     threading.Thread.__init__(self)
-    global gpsd #bring it in scope
-    gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
+    global gpsd # Bring the global variable in scope
+    gpsd = gps(mode=WATCH_ENABLE) # Calling the GPS
     self.current_value = None
-    self.running = True #setting the thread running to true
+    self.running = True # Setting the thread running to true
  
   def run(self):
     global gpsd
     while gpsp.running:
-      gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
+      gpsd.next() # This will continue to loop and grab EACH set of gpsd info to clear the buffer
 
 def usDistance(TRIG,ECHO):
   GPIO.output(TRIG, False)
@@ -73,12 +80,14 @@ def usDistance(TRIG,ECHO):
   time.sleep(0.00001)
   GPIO.output(TRIG, False)
 
+  # Get when the timestamps for start and end of ultrasonic sensor pulse
   while GPIO.input(ECHO)==0:
     pulse_start = time.time()
 
   while GPIO.input(ECHO)==1:
     pulse_end = time.time()
 
+  # Calculating the distance based on the time between 
   pulse_duration = pulse_end - pulse_start
   distance = pulse_duration * 17150
   distance = round(distance, 2)
@@ -86,21 +95,21 @@ def usDistance(TRIG,ECHO):
   if distance>2 and distance < range_limit:
     return distance
   else:
-    return "Out of range"
+    return 9999999 # Returns an extremely large value to indicate the reading is out-of-range
 
 def to_unicode(obj, encoding='utf-8'):
-    # checks if obj is a unicode string and converts if not
+    # Checks if obj is a unicode string and converts if not
     if isinstance(obj, basestring):
         if not isinstance(obj, unicode):
             obj = unicode(obj, encoding)
     return obj
 
 def log(log_type, values):
-  # add a timestamp to the values
+  # Add a timestamp to the values
   print (values)
   values = (str(mac),) + (str(datetime.now()),) + values
-  # sanitize values for storage
-  #values = tuple([to_unicode(x) for x in values])
+  # Sanitize values for storage
+  # values = tuple([to_unicode(x) for x in values])
   # insert values into the database
   values_str = ','.join('?'*len(values))
   print (values)
@@ -109,63 +118,72 @@ def log(log_type, values):
   cur.execute(query, values)
   conn.commit()
 
-def log_message(level, message):
+def log_message(level, message): # Define the function for logging a message
     log(0, (MESSAGE_LEVELS[level], message))
 
 
 def log_gps(latitude, longitude, timeutc, timefix, altitude, eps, epx, epv, ept, speed, climb, track, 
             mode, usreading_r, usreading_l, gyro_x, gyro_y, gyro_z, acce_x, acce_y, acce_z, temp):
-    #oui = resolve_oui(bssid)
+
     log(1, (latitude, longitude, timeutc, timefix,altitude, eps, epx, epv, ept, speed, climb, track, 
             mode, usreading_r, usreading_l, gyro_x, gyro_y, gyro_z, acce_x, acce_y, acce_z, temp))
 
-conn = sqlite3.connect(LOG_FILE)
-cur = conn.cursor()
-# build the database schema if necessary
+conn = sqlite3.connect(LOG_FILE) # Connect to the local database
+cur = conn.cursor() # Initiate the local database cursor
+
+# Build the database schema if necessary
 cur.execute('CREATE TABLE IF NOT EXISTS gpsreadings (mac TEXT, dtg TEXT, latitude TEXT, longitude TEXT, \
                                                      timeutc TEXT, timefix TEXT, altitude TEXT, \
                                                      eps TEXT, epx TEXT, epv TEXT, ept TEXT, speed TEXT, \
                                                      climb TEXT, track TEXT, mode TEXT, usreading_r TEXT, \
                                                      usreading_l TEXT, gyro_x TEXT, gyro_y TEXT, gyro_z TEXT, \
-                                                     acce_x TEXT, acce_y TEXT, acce_z TEXT, temp TEXT)')
-#cur.execute('CREATE TABLE IF NOT EXISTS usreadings (dtg TEXT, cm TEXT)')
-cur.execute('CREATE TABLE IF NOT EXISTS messages (mac TEXT, dtg TEXT, lvl TEXT, msg TEXT)')
+                                                     acce_x TEXT, acce_y TEXT, acce_z TEXT, temp TEXT)') # Table for sensor data
 
-conn.commit()
-log_message(0, 'GPS module started.')
-gpsp = GpsPoller() # create the thread
-gpsp.start() # start it up
+cur.execute('CREATE TABLE IF NOT EXISTS messages (mac TEXT, dtg TEXT, lvl TEXT, msg TEXT)') # Table for log messages
 
-# start the sniffer
+conn.commit() # Commit new tables
+
+log_message(0, 'GPS module started.') # Create a log message to indicate the GPS has started
+gpsp = GpsPoller() # Create the GPS thread
+gpsp.start() # Start the GPS up
+
+# Start the sniffer
 while True:
   try:
+    # Get the distance readings from the right and left ultrasonic sensors
     usreading_r = usDistance(TRIG_r,ECHO_r)
     usreading_l = usDistance(TRIG_l,ECHO_l)
 
+    # Get the gyroscope vector from the MPU6050
     gyro = sensor.get_gyro_data()
     gyro_x = gyro['x']
     gyro_y = gyro['y']
     gyro_z = gyro['z']
     
+    # Get the acceleration vector from the MPU6050
     acce = sensor.get_accel_data()
     acce_x = acce['x']
     acce_y = acce['y']
     acce_z = acce['z']
     
-    temp = sensor.get_temp()
+    temp = sensor.get_temp() # Get the temperature from the MPU6050
     
     data = [gpsd.fix.latitude, gpsd.fix.longitude, gpsd.utc, gpsd.fix.time, gpsd.fix.altitude, 
             gpsd.fix.eps, gpsd.fix.epx, gpsd.fix.epv, gpsd.fix.ept, gpsd.fix.speed, gpsd.fix.climb, 
             gpsd.fix.track , gpsd.fix.mode, usreading_r, usreading_l, gyro_x, gyro_y, gyro_z, acce_x, 
-            acce_y, acce_z, temp]
-    log_gps(*data)
-    time.sleep(log_rate) #set to whatever
+            acce_y, acce_z, temp] # Define which variables are gonna be uploaded to the local database
+
+    log_gps(*data) # Upload the data to the local database
+
+    time.sleep(log_rate) # Set to whatever the log_rate is
+
   except KeyboardInterrupt:
     break
-  except:
+  except: # Log a message in case of error
     if DEBUG: print(traceback.format_exc())
     log_message(1, 'Reading error.')
     continue
-log_message(0, 'Sensor stopped.')
-conn.close()
+log_message(0, 'Sensor stopped.') # Log message to indicate the sensor data collection has stopped
+
+conn.close() # Close connection to the local database
 
